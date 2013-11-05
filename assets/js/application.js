6b2fbfe57cf4109ef26e2485ed82846c64a7da3d
Naoise Golden Santos <naoise.golden@xing.com>
2013-11-05 17:36:52 +0100

    Add gesture listener

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,7 @@
 (function() {
   // DOM elements
   var orientationContainer = document.getElementById("orientation-container");
+  var gestureContainer     = document.getElementById("gesture-container");
 
   // Event listeners
 
@@ -8,20 +9,35 @@
   // the resize event.
   var orientationEvent = "onorientationchange" in window ? "orientationchange" : "resize";
   window.addEventListener(orientationEvent, orientationChangeListener);
+  // Gestures
+  window.addEventListener("gesturestart" , gestureListener);
+  window.addEventListener("gesturechange", gestureListener);
+  window.addEventListener("gestureend"   , gestureListener);
 
   /* init
    * Initialiazies stuff.
    */
   function init() {
     orientationChangeListener();
+    gestureListener(undefined);
+  }
+
+  /* showData
+   * Generic function for printing data.
+   */
+  function showData(message, container) {
+    container.innerHTML = message;
+    console.log(message);
   }
 
   // Note that "orientationchange" and window.orientation are unprefixed in the following
   // code although this API is still vendor-prefixed browsers implementing it.
   function orientationChangeListener() {
-    var message = "the orientation of the device is now " + window.orientation;
-    orientationContainer.innerHTML = message;
-    console.log(message);
+    showData("the orientation of the device is now " + window.orientation, orientationContainer);
+  }
+
+  function gestureListener(event) {
+    showData("the gesture " + event, gestureContainer);
   }
 
   init();
