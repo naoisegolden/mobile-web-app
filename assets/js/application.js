f328016a905aa74ee6347c03f956325e3e5be911
Naoise Golden Santos <naoise.golden@xing.com>
2013-11-05 18:10:50 +0100

    Add motion and orientation listeners

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,7 +1,10 @@
 (function() {
   // DOM elements
-  var orientationContainer = document.getElementById("orientation-container");
-  var gestureContainer     = document.getElementById("gesture-container");
+  var orientationContainer  = document.getElementById("orientation-container");
+  var gestureContainer      = document.getElementById("gesture-container");
+  var accelerationContainer = document.getElementById("acceleration-container");
+  var rotationContainer     = document.getElementById("rotation-container");
+  var rotationContainer2    = document.getElementById("rotation-container-2");
 
   // Event listeners
 
@@ -13,6 +16,11 @@
   window.addEventListener("gesturestart" , gestureListener);
   window.addEventListener("gesturechange", gestureListener);
   window.addEventListener("gestureend"   , gestureListener);
+  // Device motion
+  if (window.DeviceMotionEvent !== undefined) {
+    window.addEventListener("devicemotion"     , deviceMotionListener);
+    window.addEventListener("deviceorientation", deviceOrientationListener);
+  }
 
   /* init
    * Initialiazies stuff.
@@ -38,6 +46,27 @@
 
   function gestureListener(event) {
     showData("the gesture " + event, gestureContainer);
+  }
+
+  function deviceMotionListener(event) {
+    ax = event.accelerationIncludingGravity.x;
+    ay = event.accelerationIncludingGravity.y;
+    az = event.accelerationIncludingGravity.z;
+    rotation = event.rotationRate;
+    if (rotation !== null) {
+      arAlpha = Math.round(rotation.alpha);
+      arBeta  = Math.round(rotation.beta);
+      arGamma = Math.round(rotation.gamma);
+    }
+    showData("acceleration: " + ax + ", " + ay + ", " + az, accelerationContainer);
+    showData("rotation: " + arAlpha + ", " + arBeta + ", " + arGamma, rotationContainer);
+  }
+
+  function deviceOrientationListener(event) {
+    alpha = Math.round(event.alpha);
+    beta  = Math.round(event.beta);
+    gamma = Math.round(event.gamma);
+    showData("rotation: " + alpha + ", " + beta + ", " + gamma, rotationContainer2);
   }
 
   init();
