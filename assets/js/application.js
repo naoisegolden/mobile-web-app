562b02b14c44d295d621a1f5d1bf478f50d3d4a9
Naoise Golden Santos <naoise.golden@xing.com>
2013-11-05 17:15:46 +0100

    Put events in separate functions and init function

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,11 +1,24 @@
 (function() {
+  // DOM elements
   var orientationContainer = document.getElementById("orientation-container");
+
+  // Event listeners
+  window.addEventListener("orientationchange", orientationChangeListener);
+
+  /* init
+   * Initialiazies stuff.
+   */
+  function init() {
+    orientationChangeListener();
+  }
 
   // Note that "orientationchange" and window.orientation are unprefixed in the following
   // code although this API is still vendor-prefixed browsers implementing it.
-  window.addEventListener("orientationchange", function() {
+  function orientationChangeListener() {
     var message = "the orientation of the device is now " + window.orientation;
     orientationContainer.innerHTML = message;
     console.log(message);
-  });
+  }
+
+  init();
 })();
